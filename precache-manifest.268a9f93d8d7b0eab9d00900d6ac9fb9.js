5b933b1bae02afb9062953d81b4604b8d9b9f762
Vladimir Skok <skok@vova.io>
2019-03-04 10:32:11 -0800

    add build folder

---
+++ precache-manifest.268a9f93d8d7b0eab9d00900d6ac9fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9e2a2225d8b0bb73064",
+    "url": "/react-resume/static/css/main.ff9b3a8a.chunk.css"
+  },
+  {
+    "revision": "f9e2a2225d8b0bb73064",
+    "url": "/react-resume/static/js/main.f9e2a222.chunk.js"
+  },
+  {
+    "revision": "9ce08e71c00fa3755713",
+    "url": "/react-resume/static/js/runtime~main.9ce08e71.js"
+  },
+  {
+    "revision": "3192304a8670b9fb85f8",
+    "url": "/react-resume/static/js/2.3192304a.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "58525850a21ef7f471eba7c3dd493bf8",
+    "url": "/react-resume/index.html"
+  }
+];
