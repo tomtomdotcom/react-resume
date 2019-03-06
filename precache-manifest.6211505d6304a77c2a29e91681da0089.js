f936527a59ed319c725b9ddc561dc7e922cf1076
Vladimir Skok <skok@vova.io>
2019-03-06 10:37:53 -0800

    make changes to github pages workflow

---
+++ precache-manifest.6211505d6304a77c2a29e91681da0089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a130e9b191004027e94",
+    "url": "/react-resume/static/css/main.223050fe.chunk.css"
+  },
+  {
+    "revision": "9a130e9b191004027e94",
+    "url": "/react-resume/static/js/main.9a130e9b.chunk.js"
+  },
+  {
+    "revision": "9ce08e71c00fa3755713",
+    "url": "/react-resume/static/js/runtime~main.9ce08e71.js"
+  },
+  {
+    "revision": "ec553454222a57d5c9b5",
+    "url": "/react-resume/static/js/2.ec553454.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6a9cb27d4dc9bd593d8e0dcc13a857b4",
+    "url": "/react-resume/index.html"
+  }
+];
